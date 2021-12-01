03edd2ccdfc7c76a3d411969dacc7b09f89b9ace
daxtonparke <daxtonparke@gmail.com>
2021-12-01 16:38:40 -0700

    complete

--- swapi/main.js
+++ swapi/main.js
@@ -2,16 +2,24 @@ let button = document.querySelector('button')
 
 let baseURL = 'https://swapi.dev/api/'
 
-const residentLoop = (characters) => {
-for (i = 0; i<characters.length; i++){
-    
-}
-}
 
 const buttonFunc = () => {
-   event.preventDefault()
+    event.preventDefault()
     axios.get('https://swapi.dev/api/planets?search=Alderaan').then((res)=>{
-console.log(res.data)})}
-
+        let resArr = res.data.results[0].residents
+        residentLoop(resArr)
+        // console.log(res.data.results[0].residents)})
+    })}
+    
+    const residentLoop = arr => {
+    for (i = 0; i<arr.length; i++){
+    axios.get(arr[i]).then((res)=>{
+        // console.log(res.data)
+        let newCharacter = document.createElement('h2')
+        newCharacter.textContent = res.data.name
+        document.body.append(newCharacter)
+    })
+    }
+    }
 
 button.addEventListener('click', buttonFunc)
