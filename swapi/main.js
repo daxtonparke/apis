79d81f40424468272007580898f428833ec786e8
daxtonparke <daxtonparke@gmail.com>
2021-12-01 15:56:46 -0700

    q2

--- swapi/main.js
+++ swapi/main.js
@@ -1,6 +1,17 @@
 let button = document.querySelector('button')
 
-const buttonFunc = () => console.log('button clicked')
+let baseURL = 'https://swapi.dev/api/'
+
+const residentLoop = (characters) => {
+for (i = 0; i<characters.length; i++){
+    
+}
+}
+
+const buttonFunc = () => {
+   event.preventDefault()
+    axios.get('https://swapi.dev/api/planets?search=Alderaan').then((res)=>{
+console.log(res.data)})}
 
 
 button.addEventListener('click', buttonFunc)
